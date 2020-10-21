b55df0c4b254447f101fb23bf7346b93a7b1ff0b
Christine Pham <70411204+cpham25101@users.noreply.github.com>
2020-10-21 13:26:55 -0500

    Delete SQLQuery1 - AdventureWorks.sql

--- SQLQuery1 - AdventureWorks.sql
+++
@@ -1,9 +0,0 @@
-select * from Person.Person
-join HumanResources.Employee
-on Person.BusinessEntityID = Employee.BusinessEntityID
-join sales.SalesPerson
-on Person.BusinessEntityID = SalesPerson.BusinessEntityID
-join sales.SalesTerritory
-on SalesPerson.TerritoryID = SalesTerritory.TerritoryID
-
-
